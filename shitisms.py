c6b466fa9d1f93451324bac90f46b1b1f98cee4a
Steven Douglas <elstevi@gmail.com>
2016-05-13 15:02:38 -0700

    Made it able to display bofh, and rickyisms

--- shitisms.py
+++ shitisms.py
@@ -5,28 +5,36 @@ import random
 app = Flask(__name__)
 
 # Simple output if people come in the web browser. Don't even send a header.
-@app.route('/')
-def http():
-    return get_shit()
+@app.route('/', defaults={'type': "shitism"})
+@app.route('/<type>')
+def http(type):
+    return get_shit(type)
 
 # CLI is pretty well the same, except has a newline
-@app.route('/cli')
-def cli():
-    return get_shit()+"\n"
+@app.route('/cli', defaults={'type': "shitism"})
+@app.route('/cli/<type>')
+def cli(type):
+    return get_shit(type)+"\n"
 
 
-@app.route('/chat', methods=['POST'])
-def rocketchat():
+@app.route('/chat', methods=['POST'], defaults={'type': "shitism"})
+@app.route('/chat/<type>', methods=['POST'])
+def rocketchat(type):
     rtrn_json={}
-    rtrn_json['text'] = get_shit()
+    rtrn_json['text'] = get_shit(type)
     resp = Response(response=json.dumps(rtrn_json), status=200, mimetype="application/json")
     return resp
 
 # Get shit function
-def get_shit():
+def get_shit(type):
+    if (type == "rickyism"):
+	return random.choice(open('rickyisms.txt').read().splitlines())
+    elif (type == "bofh"):
+	return random.choice(open('bofh.txt').read().splitlines())
+    else:
 	return random.choice(open('shitisms.txt').read().splitlines())
 
 
 # Start the flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
